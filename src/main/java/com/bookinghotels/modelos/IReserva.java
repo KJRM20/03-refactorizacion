d1313f774471ebfdec846d751d83b3858921b798
Karen Rincón <karenrinconjm2016@gmail.com>
2024-12-18 16:40:03 -0500

    feat(IReserva): agregar interfaz IReserva

---
+++ src/main/java/com/bookinghotels/modelos/IReserva.java
@@ -0,0 +1,8 @@
+package com.bookinghotels.modelos;
+
+public interface IReserva {
+    public void agregarReserva();
+    public void eliminarReserva();
+    public void actualizarReserva();
+    public void mostrarReserva();
+}
